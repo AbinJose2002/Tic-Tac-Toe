c9402ef2b30e606de9d46a6aad29737686b04ab9
abinjos307@gmail.com <abinjos307@gmail.com>
2024-02-05 23:13:40 +0530

    Full features added

---
+++ app.js
@@ -0,0 +1,62 @@
+let boxes = document.querySelectorAll(".btn")
+let msgSection = document.querySelector(".winner")
+let reset = document.querySelector(".reset")
+let msg = document.querySelector(".win-para")
+playerO = true
+
+const winPattern = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+reset.addEventListener("click",()=>{
+    playerO = true
+    msgSection.classList.add("hide")
+    boxes.forEach((box)=>{
+        box.disabled =false
+        box.innerText = ""
+    })
+})
+
+const winAlert = (winner) => {
+    boxes.forEach((box)=>{
+        box.disabled =true
+        msgSection.classList.remove("hide")
+    })
+    msg.innerHTML = `Congratulation! ${winner}`
+}
+
+const checkWinner = () =>{
+    for(patt of winPattern){
+        pos1Val = boxes[patt[0]].innerText
+        pos2Val = boxes[patt[1]].innerText
+        pos3Val = boxes[patt[2]].innerText
+        if(pos1Val!="" && pos2Val!="" && pos3Val!=""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                winAlert(pos1Val);
+            }
+        }
+        // console.log(boxes[patt[0]],boxes[patt[1]],boxes[patt[2]])
+    }
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        if(playerO){
+            box.innerText = "X"
+            playerO = false
+            box.disabled = true  
+        }else{
+            box.innerText = "O"
+            playerO = true
+            box.disabled = true
+        }
+        checkWinner();
+    })
+})
